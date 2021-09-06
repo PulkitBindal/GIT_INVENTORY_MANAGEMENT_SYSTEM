58720768d16ecd62f7129df569921d998e461ec1
PulkitBindal <67583540+PulkitBindal@users.noreply.github.com>
2021-09-06 15:19:51 +0530

    Add files via upload

---
+++ adding_new_quantities.py
@@ -0,0 +1,42 @@
+import json 
+
+fd = open("record.json",'r')
+
+r = fd.read()
+
+fd.close()
+
+print("Available Inventory Products :")
+
+print(r) 
+
+record = json.loads(r)
+
+prod_id = str(input("Enter Product ID:"))
+
+name = str(input("Enter Name:"))
+
+pr = int(input("Enter Price:"))
+
+qn = int(input("Enter Quantity:"))
+
+weight = int(input("Enter Weight:"))
+
+diameter = int(input("Enter Diameter:"))
+
+material = str(input("Enter Material:"))
+
+
+record[prod_id] = {'product_name': name, 'product_price': pr, 'product_quantity': record[prod_id]['qn']+qn, 'product_weight': weight, 'product_diameter' : diameter, 'product_material' : material}
+
+js = json.dumps(record)
+
+fd = open("record.json",'w')
+
+fd.write(js)
+
+fd.close()
+
+print("------------------------------")
+print("INVENTORY STOCK HAS BEEN UPDATED SUCCESSFULLY.")
+print("------------------------------")
